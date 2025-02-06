c61afd8477da6b8d4998ff226bdaf9ee1d0a1022
an-payalmemane <payal.m@applicationnexus.com>
2025-02-06 19:30:28 +0530

    Initial commit

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,62 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import {FormsModule, ReactiveFormsModule, FormControl, FormGroup, Validators} from '@angular/forms';
+
+import { CommonModule } from '@angular/common';
+@Component({
+  selector: 'app-user',
+  imports: [CommonModule,FormsModule,ReactiveFormsModule],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+export class UserComponent  {
+  // name1 = 'John Doe';
+  // color = 'blue';
+  // email1 = '';
+  // userFunc(){
+  //   //alert( "Hello User");
+  //   alert("Hello User");
+  // }
+  // textColor= 'red';
+  // fontSize= '30px';
+  // isHighlight= false;
+  // isBold=false;
+  // toggle(){
+  //   this.isHighlight= !this.isHighlight;
+  //   this.isBold = !this.isBold;
+  // }
+  // Fruits=["apple", "Banana", "Mango", "Pineaple"];
+  // isVisible=false;
+  // textFunc(){
+  //   this.isVisible=!this.isVisible;
+  // }
+  // @Input() msg: string | undefined;
+  // @Output() childmsg = new EventEmitter<string>();
+
+  // msgFunc(){
+  //   this.childmsg.emit("This is child.");
+  // }
+  
+  user = new FormGroup({
+    name: new FormControl("",[Validators.required, Validators.minLength(3)]),
+    username: new FormControl("",[Validators.required,Validators.minLength(5)]),
+    email: new FormControl("",[Validators.required, Validators.email]),
+    password: new FormControl("",[Validators.required, Validators.minLength(8)]),
+    confirmPass: new FormControl("",[Validators.required, Validators.minLength(8)])
+  });
+  passwordsMatch(): boolean {
+    return this.user.get('password')?.value === this.user.get('confirmPass')?.value;
+  }
+  get formControls(){
+    return this.user.controls;
+
+  }
+  
+  userForm(){
+    if(this.user.valid){
+      alert("Login SuccessFully ");
+    }
+    this.user.reset();
+  }
+
+}
+
